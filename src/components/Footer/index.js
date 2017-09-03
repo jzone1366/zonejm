9683c492bb47bbcd0994db0fc5c3e44cf4755092
Joshua Zone <zonejm@localhost.localdomain>
2017-09-02 20:55:40 -0700

    Use react. from CRA

---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classNames from './Footer.css';
+
+function Footer(props) {
+    return (
+        <div> FOOTER </div>
+    )
+}
+
+export default Footer;
---
+++ src/pages/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import classNames from './App.css';
+import Footer from '../../components/Footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2> Welcome! </h2>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
